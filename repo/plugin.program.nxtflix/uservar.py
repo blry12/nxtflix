3f57ce4d949f3a0aa60e9b74ad85e647176379c9
blry12 <bradl79@comcast.net>
2024-07-10 17:06:10 -0500

    aoi wiz
    aoiwiz

---
+++ repo/plugin.program.nxtflix/uservar.py
@@ -0,0 +1,10 @@
+import xbmcaddon
+
+'''#####-----Build File-----#####'''
+buildfile = 'https://auraflix.io/nxtflix/texts/Builds.xml'
+
+'''#####-----Notifications File-----#####'''
+notify_url  = 'https://auraflix.io/nxtflix/texts/wizard_notify.txt'
+
+'''#####-----Excludes-----#####'''
+excludes  = ['plugin.video.whatever']
