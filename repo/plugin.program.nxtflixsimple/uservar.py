ea7a5d3e49d326e585b50242f2c9a80083272501
BLRY12 <bradl79@comcast.net>
2025-01-08 16:55:41 -0600

    Added Wizard
    Added NXTFlix Simple Wizard 1.0

---
+++ repo/plugin.program.nxtflixsimple/uservar.py
@@ -0,0 +1,11 @@
+'''#####-----Build File-----#####'''
+buildfile = 'https://the666mafia.com/nxtflix/txt/wiz_builds.txt'
+
+'''#####-----Videos File-----#####'''
+videos_url = 'http://CHANGEME'
+
+'''#####-----Notifications File-----#####'''
+notify_url  = 'https://the666mafia.com/nxtflix/txt/wizard_notify.txt'
+
+'''#####-----Excludes-----#####'''
+excludes  = ['plugin.video.whatever']
